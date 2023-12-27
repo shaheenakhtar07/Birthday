b76f6afa3d48dc7334849e19e25744efa24b3327
Shaheen <shaheenakhtar0708@gmail.com>
2023-12-27 12:10:46 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import Card from "./Project1 Birthday Remainder/BirthdayData"
+
+const App = () => {
+  return (
+    <div><Card/></div>
+  )
+}
+
+export default App
+
---
+++ src/Project1 Birthday Remainder/BirthdayData.jsx
@@ -0,0 +1,35 @@
+import {  useState } from "react"
+import Data from "./Data"
+import "./BirthdayData.css"
+
+
+const Card = () => {
+    const[data,setData]=useState(Data)
+    const handleClick = () => {
+      setData([]);
+    };
+    
+  return (
+    <div className="Profile">
+      <section className="Card">
+        <h3 className="black">Today Birthday {data.length} 😀</h3>
+        {data.map((e, i) => {
+          return (
+            <div className="cont" key={i}>
+              <img src={e.image}></img>
+              <div className="sub-cont">
+                <label>{e.name}</label>
+                <p>{e.age}</p>
+              </div>
+            </div>
+          );
+        })}
+        <button onClick={handleClick}>Clear</button>
+      </section>
+    </div>
+    
+    
+  )
+}
+
+export default Card
